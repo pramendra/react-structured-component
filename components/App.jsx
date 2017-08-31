a78783038c473a2d06bc9ad3232696bd3eb77882
Pramendra <pramendra@gmail.com>
2017-08-31 14:44:09 +0900

    react hellow world project

---
+++ components/App.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const App = props => <h1>Hello World</h1>;
+export default App;
---
+++ components/Friends.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Friends = props => <h1>dddd1</h1>
+export default Friends;
